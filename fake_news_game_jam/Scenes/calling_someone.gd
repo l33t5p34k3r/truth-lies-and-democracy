7992c56d96ace90fcd26c9f138ad3cea9a249291
Bacholoco <153193803+Bacholoco@users.noreply.github.com>
2025-08-14 18:55:11 +0200

    First Start
    Main Game mechanics

---
+++ fake_news_game_jam/Scenes/calling_someone.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var calling_someone = %calling_someone
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_back_pressed() -> void:
+	calling_someone.hide()
---
+++ fake_news_game_jam/Scenes/game.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var incoming_news = %incoming_news
+@onready var calling_someone = %calling_someone
+@onready var internet = %internet
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_call_someone_pressed() -> void:
+	calling_someone.show()
+
+
+func _on_check_internet_pressed() -> void:
+	internet.show()
+
+
+func _on_write_news_pressed() -> void:
+	incoming_news.show()
