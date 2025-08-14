a860870d975e8af9a1ff543d0d5495db6e6b1fd3
Konstantin Petz <konstantin.petz@outlook.at>
2025-08-14 19:35:21 +0200

    changed button name

--- fake_news_game_jam/Scenes/game.gd
+++ fake_news_game_jam/Scenes/game.gd
@@ -24,6 +24,5 @@ func _on_check_internet_pressed() -> void:
 func _on_write_news_pressed() -> void:
 	incoming_news.show()
 
-
-func _on_button_pressed() -> void:
+func _on_exit_pressed() -> void:
 	get_tree().quit()
