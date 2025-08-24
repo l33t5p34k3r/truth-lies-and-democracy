eba60c9c6c5715f0759b4f24c9740942c98e9bc6
Bacholoco <153193803+Bacholoco@users.noreply.github.com>
2025-08-24 09:48:43 +0200

    set timer for messages

--- fake_news_game_jam/Scenes/internet.gd
+++ fake_news_game_jam/Scenes/internet.gd
@@ -4,26 +4,29 @@ extends Control
 
 @onready var internet = $"."
 @onready var message_scene = preload("res://social_media_message.tscn")
-
-# Reference to the VBoxContainer inside the ScrollContainer
 @onready var message_container = $SocialMedia/ScrollContainer/VBoxContainer
+@onready var timer = $Timer
 
-var timer_out = false
+var spawned_count = 0
 
-#func _ready() -> void:
-	#spawn_messages()
-func _process(delta: float) -> void:
-	if timer_out == true:
-		spawn_messages()
-		timer_out = false
-
-func spawn_messages():
-	print("Spawning", messages_count, "messages") 
-	
-	for i in range(messages_count):
-		var message = message_scene.instantiate()
-		message_container.add_child(message)   # add to VBoxContainer
+func _ready() -> void:
+	timer.start()
 
 
 func _on_timer_timeout() -> void:
-	timer_out = true
+	if spawned_count < messages_count:
+		spawn_messages()
+		spawned_count += 1
+	else:
+		timer.stop()
+
+func spawn_messages():
+	print("Spawning message", spawned_count + 1 , "of" , messages_count, "messages") 
+	
+	var message = message_scene.instantiate()
+	message_container.add_child(message)   # add to VBoxContainer
+
+
+
+func _on_back_pressed() -> void:
+	$".".hide()
